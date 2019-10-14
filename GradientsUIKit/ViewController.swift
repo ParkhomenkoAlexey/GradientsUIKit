6bb5f4345651ed707b2b969e3198569e8a2ed41e
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2019-10-15 00:26:02 +0300

    Initial Commit

---
+++ GradientsUIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GradientsUIKit
+//
+//  Created by Алексей Пархоменко on 15.10.2019.
+//  Copyright © 2019 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
